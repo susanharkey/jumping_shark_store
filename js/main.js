928174c3ae8f90464a45d84ac2bf0a294741e8cc
Flip Stewart <flipstewart@me.com>
2014-07-20 17:26:40 -0500

    :squirrel:

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  // get the items out of the json file
+    // add the items from the json file to the DOM using the template in index.html
+
+  // create a function for updating the cart total located at the bottom of `#cart` based on the items currently in the cart
+
+  // attach an event listener to all `.buttons` in the `.list-item`s to detect clicks
+    // when clicked, add the item to the table in `#cart` using the template in index.html
+    // run the function you created to update the total
+
+  // attach an event listener to all `.buttons` in the `.cart-item`s to detect clicks
+    // when clicked, remove the item from the table in `#cart`
+    // run the function you created to update the total
+
+});
